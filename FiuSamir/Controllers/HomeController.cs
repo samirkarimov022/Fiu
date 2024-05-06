ac52b6c37539f502e45519a90c996589d966b72a
KamranTalis <az.kamik.rus@gmail.com>
2024-05-06 13:49:16 +0400

    add task

---
+++ FiuSamir/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiuSamir.Controllers
+{
+	public class HomeController:Controller		
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult ContacUs()
+		{
+			return View();
+		}
+	}
+}
